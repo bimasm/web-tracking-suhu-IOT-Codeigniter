05bfc891108f1ef2f0fab48a676c93f60f8bce5d
Bima Susila Mukti <lostpower6@gmail.com>
2019-08-26 11:17:39 +0700

    login front

--- application/views/V_login.php
+++ application/views/V_login.php
@@ -23,16 +23,7 @@
 
 		<!-- Header -->
 		<div class="header bg-gradient-primary py-7 py-lg-8">
-			<div class="container">
-				<!-- <div class="header-body text-center mb-7"> -->
-					<div class="row justify-content-center text-center">
-						<div class="col-lg-5 col-md-6">
-							<h1 class="text-white">Welcome!</h1>
-							<p class="text-lead text-light">Fill this form!</p>
-						</div>
-					</div>
-					<!-- 	</div> -->
-				</div>
+
 				<div class="separator separator-bottom separator-skew zindex-100">
 					<svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
 						<polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
@@ -58,9 +49,7 @@
 							</div>
 						</div> -->
 						<div class="card-body px-lg-5 py-lg-5">
-							<div class="text-center text-muted mb-4">
-								<small>Log in </small>
-							</div>
+							
 							<form role="form">
 								<div class="form-group mb-3">
 									<div class="input-group input-group-alternative">
@@ -78,26 +67,15 @@
 										<input class="form-control" placeholder="Password" type="password">
 									</div>
 								</div>
-								<div class="custom-control custom-control-alternative custom-checkbox">
-									<input class="custom-control-input" id=" customCheckLogin" type="checkbox">
-									<label class="custom-control-label" for=" customCheckLogin">
-										<span class="text-muted">Remember me</span>
-									</label>
-								</div>
-								<div class="text-center">
-									<a href="<?php echo base_url(); ?>Channels" type="button" class="btn btn-primary my-4">Log in</a>
+								
+								<div class="text-right">
+									
+									<a href="<?php echo base_url(); ?>Channels"><button type="button" class="btn btn-primary my-4">Login</button></a>
 								</div>
 							</form>
 						</div>
 					</div>
-					<div class="row mt-3">
-						<div class="col-6">
-							<a href="#" class="text-light"><small>Forgot password?</small></a>
-						</div>
-						<div class="col-6 text-right">
-							<a href="#" class="text-light"><small>Create new account</small></a>
-						</div>
-					</div>
+					
 				</div>
 			</div>
 		</div>
