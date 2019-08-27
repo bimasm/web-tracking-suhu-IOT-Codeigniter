a4aebfb920bdb39e8811dcc4a0609af8fa4be698
Bima Susila Mukti <lostpower6@gmail.com>
2019-08-27 19:05:07 +0700

    completing font end user and admin

---
+++ application/views/admin/V_channels.php
@@ -0,0 +1,78 @@
+<div class="container-fluid mt--7">
+	<div class="row">
+		<div class="col-2">
+
+    </div>
+		<div class="col-xl-8 mb-5 mb-xl-0">
+			<h2 style="color: white">List Of Channels</h2>
+			
+			<div class="text-right">
+				<a href="<?php echo base_url(); ?>Channels/add_channels" class="btn btn-sm btn-primary"style="background-color: white;
+				color: #646ee4; align-content: right">Add Channels</a>
+			</div>
+			<br>
+
+			
+			<div class="card-header" style="border-radius: 5px">
+				<div class="row align-items-center">
+					<div class="col">
+						<p class="mb-0" style="text-align: center;">Non Tracking</p>
+						<p class="mb-0">Channels ID : 1111</p>
+					</div>
+					<div class="col text-right">
+						<a href="<?php echo base_url(); ?>/Channels/detail" class="btn btn-sm btn-primary">View</a>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="card-header" style="border-radius: 5px">
+				<div class="row align-items-center">
+					<div class="col">
+						<p class="mb-0" style="text-align: center;">Non Tracking</p>
+						<p class="mb-0">Channels ID : 1111</p>
+					</div>
+					<div class="col text-right">
+						<a href="#!" class="btn btn-sm btn-primary">View</a>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="card-header" style="border-radius: 5px">
+				<div class="row align-items-center">
+					<div class="col">
+						<p class="mb-0" style="text-align: center;">Non Tracking</p>
+						<p class="mb-0">Channels ID : 1111</p>
+					</div>
+					<div class="col text-right">
+						<a href="#!" class="btn btn-sm btn-primary">View</a>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="card-header" style="border-radius: 5px">
+				<div class="row align-items-center">
+					<div class="col">
+						<p class="mb-0" style="text-align: center;">Non Tracking</p>
+						<p class="mb-0">Channels ID : 1111</p>
+					</div>
+					<div class="col text-right">
+						<a href="#!" class="btn btn-sm btn-primary">View</a>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="card-header" style="border-radius: 5px">
+				<div class="row align-items-center">
+					<div class="col">
+						<p class="mb-0" style="text-align: center;">Non Tracking</p>
+						<p class="mb-0">Channels ID : 1111</p>
+					</div>
+					<div class="col text-right">
+						<a href="#!" class="btn btn-sm btn-primary">View</a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		
+	</div>
---
+++ application/views/admin/V_header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>
+		Tracking
+	</title>
+	<!-- Favicon -->
+	<link href="<?php echo base_url();?>/assets/img/brand/favicon.png" rel="icon" type="image/png">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+	<!-- Icons -->
+	<link href="<?php echo base_url();?>/assets/js/plugins/nucleo/css/nucleo.css" rel="stylesheet" />
+	<link href="<?php echo base_url();?>/assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" />
+	<!-- CSS Files -->
+	<link href="<?php echo base_url();?>/assets/css/argon-dashboard.css?v=1.1.0" rel="stylesheet" />
+</head>
+
+<body class="">
+	<div class="main-content">
+		<!-- Navbar -->
+		<nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
+			<div class="container-fluid">
+				<!-- Brand -->
+				<a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="./index.html">Logo</a>
+				<!-- Form -->
+
+				<!-- User -->
+				<ul class="navbar-nav align-items-center d-none d-md-flex">
+					<li class="nav-item dropdown">
+						<a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<div class="media align-items-center">
+								<span class="avatar avatar-sm rounded-circle">
+									<img alt="Image placeholder" src="<?php echo base_url();?>//assets/img/theme/team-4-800x800.jpg">
+								</span>
+								<div class="media-body ml-2 d-none d-lg-block">
+									<span class="mb-0 text-sm  font-weight-bold">Jessica Jones</span>
+								</div>
+							</div>
+						</a>
+						<div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+							<div class=" dropdown-header noti-title">
+								<h6 class="text-overflow m-0">Welcome!</h6>
+							</div>
+							<a href="./examples/profile.html" class="dropdown-item">
+								<i class="ni ni-single-02"></i>
+								<span>My profile</span>
+							</a>
+							<a href="./examples/profile.html" class="dropdown-item">
+								<i class="ni ni-settings-gear-65"></i>
+								<span>Settings</span>
+							</a>
+							<a href="./examples/profile.html" class="dropdown-item">
+								<i class="ni ni-calendar-grid-58"></i>
+								<span>Activity</span>
+							</a>
+							<a href="./examples/profile.html" class="dropdown-item">
+								<i class="ni ni-support-16"></i>
+								<span>Support</span>
+							</a>
+							<div class="dropdown-divider"></div>
+							<a href="#!" class="dropdown-item">
+								<i class="ni ni-user-run"></i>
+								<span>Logout</span>
+							</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<!-- End Navbar -->
+		<!-- Header -->
+
+		<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+			<div class="container-fluid">
+				<div class="header-body">
+					<!-- Card stats -->
+
+				</div>
+			</div>
+		</div>
