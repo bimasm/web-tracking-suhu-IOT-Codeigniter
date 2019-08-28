55dd8f668c2a6370fbcc26c073a9d27a277c673e
Novalia Widya Popy Kristanti <novalia.kristanti@sigma.co.id>
2019-08-28 15:59:49 +0700

    curl

--- application/controllers/Channels.php
+++ application/controllers/Channels.php
@@ -3,21 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Channels extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+
+	var $API = "";
+
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+		 //$this->API="http://10.10.122.94:8181/Hirest/hirest-rest"; //local
+		$this->API="https://api.thingspeak.com/"; //
+		$this->load->library(array('session','form_validation',));
+		$this->load->helper(array('url','form','security'));
+		// $logged_in = $this->session->userdata('status')=='login' && ($this->session->userdata('profil')=='4');
+		// if(!$logged_in){
+		// 	redirect('Login');
+		// }
+	}
+
 	public function index()
 	{
 		$this->load->view('V_header');
@@ -33,9 +34,12 @@ class Channels extends CI_Controller {
 	}
 	public function detail()
 	{
-		$this->load->view('V_header');
-		$this->load->view('V_detail');
-		$this->load->view('V_footer');
+		$data['channels'] = json_decode($this->curl->simple_get($this->API . 'channels/844658/feeds.json?api_key=MP8CJ627LSUOL5TV&results=6'));
+		print_r($data);
+		// $this->load->view('V_header');
+		// $this->load->view('V_detail',$data);
+		// $this->load->view('V_footer');
+
 	}
 	public function detailtracking()
 	{
@@ -43,35 +47,37 @@ class Channels extends CI_Controller {
 		$this->load->view('V_detail_tracking');
 		$this->load->view('V_footer');
 	}
-	public function getData()
-	{
-		$curl = curl_init();
+	// public function getData()
+	// {
+	// 	$curl = curl_init();
 
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => "https://api.thingspeak.com/channels/844658/feeds.json?api_key=MP8CJ627LSUOL5TV&results=6",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "GET",
-		  CURLOPT_HTTPHEADER => array(
-		    "cache-control: no-cache",
-		    "entry_id: 15358",
-		    "postman-token: 3487192e-ee69-ca20-c5d7-10922286cf2a"
-		  ),
-		));
+	// 	curl_setopt_array($curl, array(
+	// 		CURLOPT_URL => "https://api.thingspeak.com/channels/844658/feeds.json?api_key=MP8CJ627LSUOL5TV&results=6",
+	// 		CURLOPT_RETURNTRANSFER => true,
+	// 		CURLOPT_ENCODING => "",
+	// 		CURLOPT_MAXREDIRS => 10,
+	// 		CURLOPT_TIMEOUT => 30,
+	// 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	// 		CURLOPT_CUSTOMREQUEST => "GET",
+	// 		CURLOPT_HTTPHEADER => array(
+	// 			"cache-control: no-cache",
+	// 			"entry_id: 15358",
+	// 			"postman-token: 3487192e-ee69-ca20-c5d7-10922286cf2a"
+	// 		),
+	// 	));
 
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
+	// 	$response = curl_exec($curl);
+	// 	$err = curl_error($curl);
 
-		curl_close($curl);
+	// 	curl_close($curl);
 
-		if ($err) {
-		  echo "cURL Error #:" . $err;
-		} else {
-		 	$data = var_dump(json_decode($response));
-			echo $data;
-		}
-	}
+	// 	if ($err) {
+	// 		echo "cURL Error #:" . $err;
+	// 	} else {
+	// 		$data = var_dump(json_decode($response));
+	// 		echo $data;
+	// 	}
+	// }
+
+
 }
